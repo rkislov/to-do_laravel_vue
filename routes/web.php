cc6052da706bbe12d5548dff02cfcf947f3e4517
Roman@kislovs.ru <roman@kislovs.ru>
2018-07-15 16:26:35 +0500

    jwt auth

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//Route::prefix('v1')->namespace('V1')->group(function () {
+//   Route::prefix('auth')->namespace('Auth')->group(function () {
+//      Route::post('login', 'AuthController@login');
+//      Route::post('logout', 'AuthController@logout');
+//      Route::post('refresh', 'AuthController@refresh');
+//      Route::post('me', 'AuthController@me');
+//   });
+//
+//});
